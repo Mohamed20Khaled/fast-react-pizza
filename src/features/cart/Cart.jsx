e6d1674fa937b5a92fd2a07e4b4481144ea6b877
Mohamed20Khaled <mk740015@gmail.com>
2024-06-26 11:01:13 +0300

    updating cart quantities

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,10 +1,9 @@
-import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
+import EmptyCart from './EmptyCart';
 import { useDispatch, useSelector } from 'react-redux';
 import { clearCart, getCart } from './cartSlice';
-import EmptyCart from './EmptyCart';
 
 function Cart() {
   const username = useSelector((state) => state.user.username);
@@ -21,7 +20,7 @@ function Cart() {
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
-          <CartItem item={item} key={item.id} />
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
